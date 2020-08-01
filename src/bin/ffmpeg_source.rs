963dcf9afbb7790dc11ecf8172a6da876c89cbbc
Mike L <cl.jeremy@qq.com>
2020-08-02 01:16:50 +0200

    Fix build, add speed option, add caveats

--- src/bin/ffmpeg_source.rs
+++ src/bin/ffmpeg_source.rs
@@ -8,6 +8,7 @@ use std::path::Path;
 pub struct FfmpegDecoder {
     input_context: ffmpeg::format::context::Input,
     frames: u64,
+    speed: f32,
     pts_frame_step: f64,
     min_pts: f64,
 }
@@ -22,15 +23,23 @@ impl Source for FfmpegDecoder {
 }
 
 impl FfmpegDecoder {
-    pub fn new(path: &Path, fps: f32) -> BinResult<Self> {
+    pub fn new(path: &Path, fps: f32, speed: f32) -> BinResult<Self> {
         ffmpeg::init().map_err(|e| format!("Unable to initialize ffmpeg: {}", e))?;
         let input_context = ffmpeg::format::input(&path)
             .map_err(|e| format!("Unable to open video file {}: {}", path.display(), e))?;
         // take fps override into account
-        let frames = input_context.streams().best(ffmpeg::media::Type::Video).ok_or("The file has no video tracks")?.frames() as u64;
+        let stream = input_context.streams().best(ffmpeg::media::Type::Video).ok_or("The file has no video tracks")?;
+        let rate = stream.avg_frame_rate().numerator() as f64 / stream.avg_frame_rate().denominator() as f64;
+        let max_fps = rate as f32 * speed;
+        if fps > max_fps {
+            eprintln!("Target frame rate is at most {:.2}fps based on the average frame rate of input source.\n\
+                       The specified target will net be reached. It will still be used to filter out higher-speed portions in the input.", max_fps=max_fps);
+        }
+        let frames = stream.frames() as u64;
         Ok(Self {
             input_context,
-            frames,
+            frames: (frames as f64 / rate * match fps > max_fps { true => max_fps, false => fps } as f64 / speed as f64) as u64,
+            speed,
             pts_frame_step: 1.0 / fps as f64,
             min_pts: 0.0,
         })
@@ -47,7 +56,6 @@ impl FfmpegDecoder {
         };
 
         let mut i = 0;
-        let mut prev_pts = 0;
         for (s, packet) in self.input_context.packets() {
             if s.index() != stream_index {
                 continue;
@@ -69,9 +77,10 @@ impl FfmpegDecoder {
                 stride,
             );
 
-            let pts = vid_frame.pts().unwrap_or(prev_pts + 1);
-            prev_pts = pts;
-            let ptsf = (pts as u64 * time_base.numerator() as u64) as f64 / f64::from(time_base.denominator());
+            let timestamp = vid_frame.timestamp();
+            vid_frame.set_pts(timestamp);
+            let pts = vid_frame.pts().unwrap();
+            let ptsf = (pts as u64 * time_base.numerator() as u64) as f64 / f64::from(time_base.denominator()) / self.speed as f64;
 
             if ptsf >= self.min_pts {
                 dest.add_frame_rgba(i, rgba_frame, ptsf)?;
