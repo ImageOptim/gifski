26b12f08c952aa28dcb9a80b2023e8234f64f4e2
Brian May <brian@bmay.org>
2020-11-25 10:40:29 -0800

    feat: adds support for animation looping of non-forever
    # Hey there o/!
    #
    # We just wanted to let you know that we care a great deal about
    # making our git history clean, maintainable and easy to access for
    # all our contributors. Commit messages are very important to us,
    # which is why we have a strict commit message policy in place.
    # Please use the following guidelines to format all your commit
    # messages:
    #
    #     <type>(<scope>): <subject>
    #     <BLANK LINE>
    #     <body>
    #     <BLANK LINE>
    #     <footer>
    #
    # Please note that:
    #  - The HEADER is a single line of max. 50 characters that
    #    contains a succinct description of the change. It contains a
    #    type, an optional scope, and a subject
    #       + <type> describes the kind of change that this commit is
    #                providing. Allowed types are:
    #             * feat (feature)
    #             * fix (bug fix)
    #             * docs (documentation)
    #             * style (formatting, missing semicolons, …)
    #             * refactor
    #             * test (when adding missing tests)
    #             * chore (maintain)
    #       + <scope> can be anything specifying the place of the commit
    #                 change
    #       + <subject> is a very short description of the change, in
    #                   the following format:
    #             * imperative, present tense: “change” not
    #               “changed”/“changes”
    #             * no capitalized first letter
    #             * no dot (.) at the end
    #  - The BODY should include the motivation for the change and
    #    contrast this with previous behavior and must be phrased in
    #    imperative present tense
    #  - The FOOTER should contain any information about Breaking
    #    Changes and is also the place to reference GitHub issues that
    #    this commit closes
    #
    # <type>(<scope>): <subject>  (Max 50 char)
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    Prior to this change, gifski had 2 modes of looping animation, once and forever.
    
    # How does it address the issue?
    This change adds the ability to not loop, or set a fixed number of loops and keeps the ability to loop forever
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Remember to
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

--- src/encoderust.rs
+++ src/encoderust.rs
@@ -29,7 +29,9 @@ impl<W: Write> Encoder for RustEncoder<W> {
             None => {
                 let w = writer.take().expect("writer");
                 let mut enc = gif::Encoder::new(w, screen_width, screen_height, &[])?;
-                if !settings.once {
+                if settings.once {
+                    enc.write_extension(gif::ExtensionData::Repetitions(gif::Repeat::Finite(settings.loop_count)))?;
+                } else {
                     enc.write_extension(gif::ExtensionData::Repetitions(gif::Repeat::Infinite))?;
                 }
                 self.gif_enc.get_or_insert(enc)
--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,8 @@ pub struct Settings {
     pub once: bool,
     /// Lower quality, but faster encode
     pub fast: bool,
+    /// If once is true, loop_count is the number of loops the animation is shown
+    pub loop_count: u16,
 }
 
 impl Settings {
